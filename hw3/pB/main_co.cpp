f497494f4855c7b357082983dfbb02a5e8a263ac
vanilasky00 <pipichun2@gmail.com>
2025-10-31 01:22:54 +0800

    add block layer parallel logic but with some value bug

--- hw3/pB/main_co.cpp
+++ hw3/pB/main_co.cpp
@@ -11,47 +11,57 @@ using namespace std;
 
 #define UPPER_BOUND 1e6
 
-static constexpr int TAG_PIVOT = 42;
+static constexpr int TAG_ROW = 42;
 
-// static inline void pack_matrix(const vector<vector<int>>& A, int n, int world_size, int me, vector<double>& buf);
-// static inline void unpack_matrix(vector<vector<int>>& A, int n, int world_size, int me, const vector<double>& buf);
-
-int solver(vector<vector<int>>& prev, int n, int t, MPI_comm comm) {
+long long solver(vector<vector<int>>& prev, int n, int t, MPI_Comm comm) {
     int world_size, me;
-    MPI_comm_size(comm, &world_size);
-    MPI_comm_rank(comm, &me);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &me);
 
-    int start_row, end_row;
-    int start_col, end_col;
-    int group = me / 2;
+    int rows_per = n / world_size;
+    int start_row = me * rows_per + 1;
+    int end_row = me == world_size - 1 ? n : start_row + rows_per - 1;
 
-    start_row = group * n + 1;
-    end_row = (group + 1) * n;
-    start_col = (me % 2) * n;
-    end_col = (me % 2 + 1) * n;
-
-    int res = 0;
     vector<vector<int>> curr(n+2, vector<int>(n+2, 0));
 
-    while(t-- > 0) {
+    int up = (me == 0)                ? MPI_PROC_NULL : me - 1;
+    int down = (me == world_size - 1) ? MPI_PROC_NULL : me + 1;
 
+    while(t-- > 0) {
         for (int i = start_row; i <= end_row; i++) {
-            for (int j = start_col; j <= end_col; j++) {
+            for (int j = 1; j <= n; j++) {
                 int v = prev[i][j] / 2 + (prev[i+1][j] + prev[i-1][j] + prev[i][j+1] + prev[i][j-1]) / 4;
                 if (v > UPPER_BOUND) v = UPPER_BOUND;
                 curr[i][j] = v;
             }
         }
         swap(prev, curr);
+
+        // Broadcast and receive
+        // top boundary
+        MPI_Sendrecv(
+            prev[start_row].data() + 1, n, MPI_INT, up, TAG_ROW,
+            prev[end_row].data() + 1, n, MPI_INT, down, TAG_ROW,
+            comm, MPI_STATUS_IGNORE);
+
+        // bottom
+        MPI_Sendrecv(
+            prev[end_row].data() + 1, n, MPI_INT, up, TAG_ROW,
+            prev[start_row].data() + 1, n, MPI_INT, down, TAG_ROW,
+            comm, MPI_STATUS_IGNORE);      
     }
 
-    for (int i = 1; i <= n; i++) {
+    long long local = 0;
+    for (int i = start_row; i <= end_row; i++) {
         for (int j = 1; j <= n; j++) {
-            res += prev[i][j];
+            local += (long long)prev[i][j];
         }
     }
 
-    return res;
+    long long total = 0;
+    MPI_Reduce(&local, &total, 1, MPI_LONG_LONG, MPI_SUM, 0, comm);
+
+    return total;
 }
 
 int main(int argc, char *argv[]) {
@@ -73,6 +83,7 @@ int main(int argc, char *argv[]) {
 
         file >> n >> t;
         A.assign(n + 2, vector<int>(n + 2, 0));  
+
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= n; ++j) {
                 file >> A[i][j];
@@ -89,7 +100,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (world_rank != 0) {
-        A.assign(n+2, vector<int>(n+2));
+        A.assign(n+2, vector<int>(n+2, 0));
     }
 
     vector<int> flat((n+2)*(n+2));
@@ -110,7 +121,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    int res = solver(A, n, t, MPI_COMM_WORLD);
+    long long res = solver(A, n, t, MPI_COMM_WORLD);
 
     if (world_rank == 0) {
         cout << res << "\n"; 
